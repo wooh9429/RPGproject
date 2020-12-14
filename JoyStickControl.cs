c3f2c7494a0023add46fd4e0f2e069afaf56aa5a
unknown <woohyung9429@gmail.com>
2020-12-14 12:24:37 +0900

    Test

---
+++ JoyStickControl.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class JoyStickControl : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    private Image bgImg;    // 조이스틱 범위 이미지
+    private Image stickImg; // 조이스틱
+    public Vector3 inputVector;
+    //public Vector3 playerVector;
+
+    public GameObject player;   //플레이어 오브젝트 추가
+    Rigidbody playerMove;
+    public float moveSpeed;
+    private bool moveCheck = false;
+
+    void Start()
+    {
+        playerMove = player.GetComponent<Rigidbody>();
+        bgImg = GetComponent<Image>();  // Image 컴퍼넌트 받아온다
+        stickImg = transform.GetChild(0).GetComponent<Image>(); // 0번째 자식의 Image 컴퍼넌트 받아온다
+    }
+
+    void Update()
+    {
+        //playerVector = inputVector;
+        if (moveCheck == true)
+        {
+            player.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            player.transform.rotation = Quaternion.Euler(0f, Mathf.Atan2(inputVector.x, inputVector.y) * Mathf.Rad2Deg, 0);
+        }
+    }
+
+    public virtual void OnDrag(PointerEventData ped)    // 드래그 했을때
+    {
+        Vector2 pos;
+
+
+        // 순서대로 rect, screenpoint, cam, out localpoint
+        // screenpoint가 rect의 local스페이스를 기준으로 어느 정도의 좌표에 있는지를 localpoint안에 담아준다
+        // 그리고 screenpoint가 rect의 내부에 있으면 true를 반환
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform,ped.position,ped.pressEventCamera,out pos))
+        {
+            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
+
+            inputVector = new Vector3(pos.x * 2, pos.y * 2, 0);
+
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;    // magnitude: 벡터의 길이를 반환 , normalized 정규화하여 1로 변환
+
+            // 조이스틱 일정범위 안에서 움직이기
+            stickImg.rectTransform.anchoredPosition = new Vector3(inputVector.x * (bgImg.rectTransform.sizeDelta.x /3), inputVector.y * (bgImg.rectTransform.sizeDelta.y /3));
+
+            // 플레이어 움직임
+            //바로 조이스틱의 벡터이다. 하지만 이 벡터는 바로 쓸수 없다.
+            //쓰기 위해선 아크탄젠트를 통해 변환하고, 나온 라디안 값을 디그리로 변환해주고 나온 디그리 값으로 플레이어의 y축 회전에 쓰면된다.
+        }
+    }
+
+    public virtual void OnPointerDown(PointerEventData ped) // 눌렀을때
+    {
+        moveCheck = true;
+        OnDrag(ped);    // 드래그 함수 호출
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped)   // 땟을때-+
+
+    {
+        moveCheck = false;
+        inputVector = Vector3.zero;
+        stickImg.rectTransform.anchoredPosition = Vector3.zero;
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,246 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    JoyStickControl joyStick;
+
+    public Animator anim;
+
+    Rigidbody rigid;
+    Quaternion dodgeVec;
+
+    bool wDown;
+    bool jDown;
+    bool isJump;
+    bool isDodge;
+    bool iDown;
+    bool sDown1;
+    bool sDown2;
+    bool sDown3;
+    bool isSwap;
+
+    public int ammo;
+    public int coin;
+    public int health;
+    public int hasGrenades;
+
+    public int maxAmmo;
+    public int maxCoin;
+    public int maxHealth;
+    public int maxHasGrenades;
+
+    int equipWeaponIndex = -1;
+    public float jumpPower;
+
+    GameObject nearObj;
+    GameObject equipWeapon;
+
+    public GameObject[] weapons;
+    public bool[] hasWeapons;
+    public GameObject[] grenades;
+
+    void Awake()
+    {
+        joyStick = GameObject.Find("JoyStick").GetComponentInChildren<JoyStickControl>();
+        anim = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        GetInput();
+        Move();
+        Jump();
+        Dodge();
+        Swap();
+        Interaction();
+    }
+
+    void Move()
+    {
+        anim.SetBool("isWalk", joyStick.inputVector != Vector3.zero);
+        anim.SetBool("isRun", wDown);
+
+        if(isSwap)
+        {
+            joyStick.moveSpeed = 0;
+        }
+        else
+        {
+            joyStick.moveSpeed = 3.5f;
+        }
+
+        if(isDodge)
+        {
+            joyStick.player.transform.rotation = dodgeVec; // Dodge하는 순간 방향 고정
+        }
+            
+        
+    }
+
+    void GetInput()
+    {
+        wDown = Input.GetButton("Walk");
+        jDown = Input.GetButtonDown("Jump");
+        iDown = Input.GetButtonDown("Interaction");
+
+        //if(hasWeapons[0])
+            sDown1 = Input.GetButtonDown("Swap1");
+
+        //if(hasWeapons[1])
+            sDown2 = Input.GetButtonDown("Swap2");
+
+        //if (hasWeapons[2])
+            sDown3 = Input.GetButtonDown("Swap3");
+    }
+
+    void Jump()
+    {
+        if(jDown && joyStick.inputVector == Vector3.zero && !isJump && !isDodge && !isSwap)
+        {
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    void Dodge()
+    {
+        if (jDown && joyStick.inputVector != Vector3.zero && !isJump && !isDodge && !isSwap)
+        {
+            dodgeVec = joyStick.player.transform.rotation;  // Dodge하는 순간 방향 고정
+            joyStick.moveSpeed *= 2;    // Dodge시 스피드 2배
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.4f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        joyStick.moveSpeed *= 0.5f; // Dodge 후 스피드 원상복귀
+        isDodge = false;
+    }
+
+    void Swap()
+    {
+        if (sDown1 && (!hasWeapons[0] || equipWeaponIndex == 0))
+            return;
+
+        if (sDown2 && (!hasWeapons[1] || equipWeaponIndex == 1))
+            return;
+
+        if (sDown3 && (!hasWeapons[2] || equipWeaponIndex == 2))
+            return;
+
+
+        int weaponIndex = -1;
+        if (sDown1) weaponIndex = 0;
+        if (sDown2) weaponIndex = 1;
+        if (sDown3) weaponIndex = 2;
+
+        if ((sDown1 || sDown2 || sDown3) && !isJump && !isDodge)
+        {
+            if(equipWeapon != null)
+            {
+                equipWeapon.SetActive(false);
+            }
+
+            equipWeaponIndex = weaponIndex;
+            equipWeapon = weapons[weaponIndex];
+            equipWeapon.SetActive(true);
+
+            anim.SetTrigger("doSwap");
+
+            isSwap = true;
+
+            Invoke("SwapOut", 0.4f);
+        }
+    }
+
+    void SwapOut()
+    {
+        isSwap = false;
+    }
+
+    void Interaction()
+    {
+        if(iDown && nearObj != null && !isJump && !isDodge)
+        {
+            if(nearObj.tag == "Weapon")
+            {
+                Item item = nearObj.GetComponent<Item>();
+                int weaponIndex = item.value;
+                hasWeapons[weaponIndex] = true;
+
+                Destroy(nearObj);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Item")
+        {
+            Item item = other.GetComponent<Item>();
+            switch (item.type)
+            {
+                case Item.Type.Ammo:
+                    ammo += item.value;
+                    if (ammo > maxAmmo)
+                        ammo = maxAmmo;
+                    break;
+
+                case Item.Type.Coin:
+                    coin += item.value;
+                    if (coin > maxCoin)
+                        coin = maxCoin;
+                    break;
+
+                case Item.Type.Heart:
+                    health += item.value;
+                    if (health > maxHealth)
+                        health = maxHealth;
+                    break;
+
+                case Item.Type.Grenade:
+                    grenades[hasGrenades].SetActive(true);
+                    hasGrenades += item.value;
+                    if (hasGrenades > maxHasGrenades)
+                        hasGrenades = maxHasGrenades;
+                    break;
+            }
+
+            Destroy(other.gameObject);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Weapon")
+        {
+            nearObj = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Weapon")
+        {
+            nearObj = null;
+        }
+    }
+}
